4416a1150014c0304bd73ea4ffa463ecd4cd5619
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-01 18:42:32 -0400

    added npmrc changed case in package.json files to fix problems

--- client/src-sw.js
+++ client/src-sw.js
@@ -36,8 +36,10 @@ new StaleWhileRevalidate({
   }),
   new ExpirationPlugin({
     maxEntries: 60,
-    maxAgeSeconds: 30 * 24 * 60 * 60,
+    maxAgeSeconds: 31536000,
     // https://developer.chrome.com/docs/workbox/modules/workbox-expiration/ #Restrict the Age of Cached Entries
+    // https://ashton.codes/set-cache-control-max-age-1-year/#:~:text=It's%20standard%20practice%20to%20set,seconds%2C%20which%20is%20one%20year.
+    // https://www.cdnetworks.com/knowledge-center/what_is_cache_control/#:~:text=Cache%2Dcontrol%3A%20max%2Dage,-The%20max%2Dage&text=It%20is%20the%20maximum%20amount,can%20be%20available%20for%20reuse.
   })
   ],
 })
