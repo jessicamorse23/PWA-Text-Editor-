bdeb83dd817c016c7d8e77a4faab89ac0fe2e71a
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-01 17:56:26 -0400

    throwing error - not sure why

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // source for code NU Module 19 - mini project database.js
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { console.error('putDb not implemented');
 
  // Create a connection to the database database and version we want to use.
 const contactDb = await openDB('jate', 1);
@@ -25,16 +25,37 @@ const tx = contactDb.transaction('jate', readwrite);
   // Open up the desired object store.
 const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
-const getAll = store.getAll();
+// Use the .getAll() method to get all data in the database.
+// const getAll = store.getAll();
 
  // Use the .add() method on the store and pass in the content.
-const request = store.put
+const request = store.put({ id: 1, value: content});
 
-
+// Get confirmation of the request.
+const result = await request;
+console.log('🚀 - data saved to the database', result);
+};
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+
+// Create a connection to the database database and version we want to use.
+const contactDb = await openDB('jate', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = contactDb.transaction('jate', 'readonly');
+
+// Open up the desired object store.
+const store = tx.objectStore('jate');
+
+// Use the .getAll() method to get all data in the database.
+const request = store.getAll();
+
+// Get confirmation of the request.
+const result = await request;
+console.log('result.value', result);
+return result;
+
 
 initdb();
